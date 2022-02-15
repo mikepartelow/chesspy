c053eb823d419c34579a7dc4e179b6c6a900170c
Mike Partelow <github@mikepartelow.com>
2022-02-14 21:13:24 -0800

    parallel testing

--- app/tests/test_pgn.py
+++ app/tests/test_pgn.py
@@ -1,9 +1,11 @@
 import os
+import glob
 import datetime
 import unittest
 import itertools
 import chesspy.game
 from chesspy import pgn
+from multiprocessing import Process
 
 def board_reprs(path):
     with open(path, 'r') as f:
@@ -80,47 +82,49 @@ class TestMagnusLichess(unittest.TestCase):
     # downlaod the file at: https://lichess.org/@/DrNykterstein/download
     # could use any lichess pgn file
 
-    def exec_test_pgn(self, basename, game_count=1, do_print=False, continue_on_fail=False):
+    def exec_test_pgn(self, basename, do_print=False, continue_on_fail=False):
         pgnfile = f"tests/games/{basename}.pgn"
-        metadata_file = f"tests/games/ignore.metadata.{basename}.pgn"
         failure_file = f"tests/games/failures.metadata.{basename}.pgn"
 
         if os.path.exists(pgnfile):
             with open(failure_file, "w") as failure_f:
                 failure_f.truncate()
 
-            with open(metadata_file, "w") as metadata_f:
-                game_count_actual = 0
+            for pgn_game in pgn.Gamefile(pgnfile):
+                game = chesspy.game.Game()
+                for move in pgn_game:
+                    if do_print:
+                        print(f"{int(move.idx/2 + 1)}. {game.turn}: {move.sanstr}")
 
-                for pgn_game in pgn.Gamefile(pgnfile):
-                    game = chesspy.game.Game()
-                    metadata_f.write('[Site "{}"]\n'.format(pgn_game.metadata.site))
-                    game_count_actual += 1
-                    for move in pgn_game:
-                        if do_print:
-                            print(f"{int(move.idx/2 + 1)}. {game.turn}: {move.sanstr}")
+                    try:
+                        game.move_san(move.sanstr)
+                    except (AssertionError, IndexError) as e:
+                        with open(failure_file, "a") as failure_f:
+                            failure_f.write('[Site "{}"]\n'.format(pgn_game.metadata.site))
 
-                        try:
-                            game.move_san(move.sanstr)
-                        except (AssertionError, IndexError) as e:
-                            with open(failure_file, "a") as failure_f:
-                                failure_f.write('[Site "{}"]\n'.format(pgn_game.metadata.site))
+                        if continue_on_fail:
+                            break
+                        else:
+                            raise
 
-                            if continue_on_fail:
-                                break
-                            else:
-                                raise
+                    if do_print:
+                        print(game.board)
+                        print(f"|{repr(game.board)}|")
+                        print("")
 
-                        if do_print:
-                            print(game.board)
-                            print(f"|{repr(game.board)}|")
-                            print("")
+    def test_long(self):
+        if (long_basename := os.environ.get('TEST_LONG', None)):
+            filenames = glob.glob(f"tests/games/{long_basename}.*.pgn")
+            filenames = [ os.path.basename(n).split('.pgn')[0] for n in filenames ]
 
-            self.assertEqual(game_count, game_count_actual)
+            procs = []
+            for filename in filenames:
+                p = Process(target=self.exec_test_pgn, args=(filename,),
+                                                       kwargs={'do_print': False, 'continue_on_fail': True})
+                p.start()
+                procs.append(p)
 
-    def test_DrNykterstein(self):
-        if os.environ.get('PYCHESS_TEST_LONG_TEST', None) == 'YES':
-            self.exec_test_pgn('lichess_DrNykterstein_2022-01-04', game_count=9664, do_print=True, continue_on_fail=True)
+            [ p.join() for p in procs ]
 
     def test_n7ZjoKNR(self):
         self.exec_test_pgn('n7ZjoKNR')
