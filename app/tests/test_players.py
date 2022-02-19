61f7639aca607ac48c8438218a7d58e243b77482
Mike Partelow <github@mikepartelow.com>
2022-02-18 22:23:50 -0800

    itertools

--- app/tests/test_players.py
+++ app/tests/test_players.py
@@ -1,4 +1,5 @@
 import unittest
+import itertools
 from chesspy import players
 from chesspy.game import Game
 from chesspy.color import Color
@@ -9,24 +10,16 @@ class TestRandy(unittest.TestCase):
         game = Game()
         player_w = players.Randy(game)
         player_b = players.Randy(game)
+        moves = 100
 
         with open("tests/games/randy_v_randy.txt", "w") as game_file:
-            for turn in range(50):
-                if game.over:
+            for player in itertools.cycle((player_w, player_b)):
+                if game.over or moves < 0:
                     break
+                moves -= 1
 
-                sanstr = player_w.suggest_move_san()
+                sanstr = player.suggest_move_san()
                 game_file.write(f"{repr(game.board)}\n")
                 game_file.write(sanstr + "\n")
 
                 game.move_san(sanstr)
-
-                if game.over:
-                    break
-
-                sanstr = player_b.suggest_move_san()
-                game_file.write(f"{repr(game.board)}\n")
-                game_file.write(sanstr + "\n")
-
-                game.move_san(sanstr)
-
